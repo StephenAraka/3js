1fd0d55ad5758ddc63dd1f2cf4f059da034a30b4
Stephen Araka <stephen@Stephens-MacBook-Pro.local>
2024-10-22 23:34:26 +0300

    Animate color on mouse movement

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ const geometry = new THREE.SphereGeometry(3, 64, 64);
 
 const material = new THREE.MeshStandardMaterial({
   color: '#00ff83',
+  roughness: 0.5,
 });
 
 const mesh = new THREE.Mesh(geometry, material);
@@ -26,6 +27,7 @@ const sizes = {
 // Add light
 const light = new THREE.PointLight(0xffffff, 100, 100);
 light.position.set(0, 10, 10);
+// light.intensity = 1.25;
 scene.add(light);
 
 // 3. Add camera
@@ -38,6 +40,7 @@ scene.add(camera);
 const canvas = document.querySelector('.webgl');
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(2);
 renderer.render(scene, camera);
 
 // Controls
@@ -73,3 +76,23 @@ const tl = gsap.timeline({ defaults: { duration: 1 }});
 tl.fromTo(mesh.scale, { z:0, x:0, y:0 }, { z:1, x:1, y:1 });
 tl.fromTo('nav', { y: "-100%" }, { y: "0%" });
 tl.fromTo('.title', { opacity: 0 }, { opacity: 1 });
+
+// Mouse Animation Colorrr
+let mouseDown = false;
+let rgb = [];
+window.addEventListener('mousedown', () => (mouseDown = true));
+window.addEventListener('mouseup', () => (mouseDown = false));
+
+window.addEventListener('mousemove', (e) => {
+  if (mouseDown) {
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pageY / sizes.height) * 255),
+      150
+    ]
+  }
+
+  // Let's animate
+  let newColor = new THREE.Color(`rgb(${rgb.join(',')})`);
+  gsap.to(mesh.material.color, { r: newColor.r, g: newColor.g, b: newColor.b })
+});
